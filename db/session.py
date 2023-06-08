e03888d9eff2f013990e7a9ca095f9432cf331a0
Igor Deminski <kartoschka17gmail.com>
2023-06-08 12:03:27 +0300

    realization endpoints to manage user data + tests

--- db/session.py
+++ db/session.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import sessionmaker
 import settings
 
 # create async engine for interaction with database
-engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
+engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True,
+                             execution_options={"isolation_level": "AUTOCOMMIT"})
 
 # create session for the interaction with database
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
