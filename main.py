65236e80a4045c534e35e885126bb445066bf812
Igor Deminski <kartoschka17gmail.com>
2023-06-10 12:51:25 +0300

    refactor structure, more tests

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ app.include_router(main_api_router)
 
 if __name__ == "__main__":
     # run app on the host and port
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
